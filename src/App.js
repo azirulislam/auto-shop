8d032f6290bd9d99a6215b5915576bf127d051b8
azirulislam <azirulislam27@gmail.com>
2022-05-02 00:48:49 +0600

    working for home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Home from './components/Home/Home';
 function App() {
   return (
     <div className="App">
       <h2>Hellow World</h2>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
