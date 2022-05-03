1403248d57250dd59e488c321ddff7eb2da91584
azirulislam <azirulislam27@gmail.com>
2022-05-04 01:42:19 +0600

    404 page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Inventory from './components/Inventory/Inventory';
 import Manage from './components/Manage/Manage';
 import Blogs from './components/Blogs/Blogs';
 import Header from './components/Header/Header';
+import Notfound from './components/Notfound/Notfound';
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/" element={<Inventory></Inventory>}></Route>
-        <Route path="/" element={<Manage></Manage>}></Route>
-        <Route path="/" element={<Blogs></Blogs>}></Route>
+        <Route path="/Home" element={<Home></Home>}></Route>
+        <Route path="/Inventory" element={<Inventory></Inventory>}></Route>
+        <Route path="/Manage" element={<Manage></Manage>}></Route>
+        <Route path="/Blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="*" element={<Notfound></Notfound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Notfound.css';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h5 className='text-danger mt-5 text-center'>Sorry! You are nothing to found.</h5>
+        </div>
+    );
+};
+
+export default Notfound;
