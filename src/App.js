e4621ab05915c69cd6fcb7bb19add5fcd066d97b
azirulislam <azirulislam27@gmail.com>
2022-05-04 00:51:22 +0600

    added manage and blog page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import Inventory from './components/Inventory/Inventory';
+import Manage from './components/Manage/Manage';
+import Blogs from './components/Blogs/Blogs';
 function App() {
   return (
     <div className="App">
       <h2>Hellow World</h2>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/" element={<Inventory></Inventory>}></Route>
+        <Route path="/" element={<Manage></Manage>}></Route>
+        <Route path="/" element={<Blogs></Blogs>}></Route>
       </Routes>
     </div>
   );
